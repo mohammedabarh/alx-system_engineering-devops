d7708b56a5fc4a55eb9b975c726ef58048e6b945
mohammedabarh <abmohammed606@gmail.com>
2024-11-01 17:29:46 -0700

    0x05-processes_and_signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,39 +4,46 @@
 
 /**
  * infinite_while - Creates an infinite loop
- * Return: 0
+ * Description: Function that keeps the program running
+ * Return: Always 0 (Success)
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
  * main - Creates 5 zombie processes
- * Return: 0
+ * Description: Program that demonstrates zombie process creation
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t zombie_pid;
+	unsigned int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid == 0)
-        {
-            exit(0);
-        }
-        else
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-        }
-    }
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+		if (zombie_pid < 0)
+		{
+			perror("fork");
+			return (1);
+		}
+		if (zombie_pid == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", zombie_pid);
+		}
+	}
 
-    infinite_while();
-    return (0);
+	infinite_while();
+	return (0);
 }
