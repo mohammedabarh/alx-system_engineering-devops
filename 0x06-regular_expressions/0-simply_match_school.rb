4050a0f49271b21da7398474413b0bd734678be0
mohammedabarh <abmohammed606@gmail.com>
2024-11-05 08:24:20 -0800

    0x06-regular_expressions

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
+# Script Name: 0-simply_match_school.rb
+# Description: Matches the string "School" in the input text
+# Author: Your Name
+# Date: Current Date
+
 puts ARGV[0].scan(/School/).join
