8977dc84573c25b2e276e674a2d24cda103168d8
mohammedabarh <abmohammed606@gmail.com>
2025-01-15 16:48:14 -0800

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,30 @@
-# Install Nginx server, setup and configuration
+# This Puppet script installs and configures Nginx to serve a "Hello World!" page
+# and sets up a redirect for /redirect_me to a specified URL.
 
+# Install Nginx package
 package { 'nginx':
   ensure => 'installed',
 }
 
-# Create the index.html file with "Hello World"
+# Create the index.html file with "Hello World!" content
 file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World',
-}
-
-# Configure Nginx to redirect /redirect_me
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+  ensure  => file,
+  content => 'Hello World!',
   require => Package['nginx'],
 }
 
-# Ensure the Nginx service is running
+# Configure Nginx to redirect /redirect_me to a specified URL
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@cryptoappcodergroup permanent;',
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx service is running
 service { 'nginx':
-  ensure => running,
-  enable => true,  # Ensure Nginx starts on boot
-  subscribe => File['/etc/nginx/sites-available/default'],  # Restart if the config file changes
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
