16fa82786b281b8a0c2ae893a2890f4d37b7858a
mohammedabarh <abmohammed606@gmail.com>
2025-02-27 00:57:00 +0000

    0x17-web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# 0-strace_is_your_friend.pp
+# Fixes a WordPress file that causes Apache to return 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
