ef7a7af98ee8972a2439e6e3b8049208db22de0c
mohammedabarh <abmohammed606@gmail.com>
2025-02-27 01:27:45 +0000

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
-# Manifest to fix apache configuration
-exec { 'fix config file':
-  command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin'
+# 0-strace_is_your_friend.pp
+# Fixes a WordPress file that causes Apache to return 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
