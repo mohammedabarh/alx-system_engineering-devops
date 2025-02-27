4c8e37d91f26708dd18d6208575d1a4373839173
mohammedabarh <abmohammed606@gmail.com>
2025-02-27 02:31:55 +0000

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,36 @@
-# fix error
+# 0-strace_is_your_friend.pp
+# Fixes a WordPress file that causes Apache to return 500 error
 
-exec { 'fix-error':
-  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/var/www/html/wp-settings.php',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
 
+# This Puppet manifest fixes the 500 Internal Server Error for Apache on a WordPress site
+
+# Ensure the required PHP module is installed
+package { 'php5-mysql':
+  ensure => installed,
+}
+
+# Ensure the Apache service is running
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+# Set the correct permissions for the WordPress directory
+file { '/var/www/html':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+# Restart Apache to apply changes
+exec { 'restart_apache':
+  command => '/usr/sbin/service apache2 restart',
+  refreshonly => true,
+  subscribe => File['/var/www/html'],
+}
