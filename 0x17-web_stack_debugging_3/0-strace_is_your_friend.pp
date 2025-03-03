6ea837e9e2e172bf67f71fbc8b9c2427127b5244
mohammedabarh <abmohammed606@gmail.com>
2025-03-03 02:28:31 +0000

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes Apache 500 error by correcting PHP file extension in WordPress settings
 exec { 'fix-wordpress':
-  command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/\.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
