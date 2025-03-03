b2009e9fa208cfbbd38af764cbd8accdacce6196
mohammedabarh <abmohammed606@gmail.com>
2025-03-03 03:16:17 +0000

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,9 @@
 # 0-strace_is_your_friend.pp
-# Fix for WordPress 500 error
+# This Puppet manifest fixes an Apache 500 error in a WordPress installation
+# by correcting a typo in the wp-settings.php file where "php" was misspelled as "phpp"
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
 }
